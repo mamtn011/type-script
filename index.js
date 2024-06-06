0101dfb674eec38a4a13459a36d2a5dda9496ecc
Mobin <mamtn011@gmail.com>
2024-06-06 14:59:17 +0600

    working with basic types

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+"use strict";
 // add 2 number
-var sum = function (num1, num2) {
+const sum = (num1, num2) => {
     return num1 + num2;
 };
-console.log(sum(12, 10));
+// console.log(sum(12, 10));
+const longText = "A quick brown fox jumps over the lazy dog";
+const shortText = longText.substring(0, 17);
+// console.log(shortText);
+const isEqual = longText == shortText;
+// console.log(isEqual);
+// console.log(typeof isEqual);
+const productCode = "SD-109";
+const price = 2490;
+const productDetails = `The price of "${productCode}" is: ${price}`;
+// console.log(productDetails);
+const isDivisible = (num) => num % 4 === 0 && num % 8 === 0;
+// console.log(isDivisible(20));
+const bigNum1 = 9007199254740999n;
+const bigNum2 = 9007199254740991n;
+const difference = bigNum1 - bigNum2;
+const bigNumMake = BigInt(2 ** 54);
+console.log(bigNumMake);
--- index.ts
+++ index.ts
@@ -2,5 +2,26 @@
 const sum = (num1: number, num2: number): number => {
   return num1 + num2;
 };
+// console.log(sum(12, 10));
 
-console.log(sum(12, 10));
+const longText: string = "A quick brown fox jumps over the lazy dog";
+const shortText: string = longText.substring(0, 17);
+// console.log(shortText);
+
+const isEqual: boolean = longText == shortText;
+// console.log(isEqual);
+// console.log(typeof isEqual);
+
+const productCode: string = "SD-109";
+const price: number = 2490;
+const productDetails: string = `The price of "${productCode}" is: ${price}`;
+// console.log(productDetails);
+
+const isDivisible = (num: number): boolean => num % 4 === 0 && num % 8 === 0;
+// console.log(isDivisible(20));
+
+const bigNum1: bigint = 9007199254740999n;
+const bigNum2: bigint = 9007199254740991n;
+const difference: bigint = bigNum1 - bigNum2;
+const bigNumMake: bigint = BigInt(2 ** 54);
+// console.log(bigNumMake);
