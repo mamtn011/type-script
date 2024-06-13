c1543bf92db3f28d56f7aebc3c0d7a24ea49f036
Mobin <mamtn011@gmail.com>
2024-06-14 01:55:28 +0600

    object, type alias, call sinatures

--- index.js
+++ index.js
@@ -14,12 +14,6 @@ const productCode = "SD-109";
 const price = 2490;
 const productDetails = `The price of "${productCode}" is: ${price}`;
 // console.log(productDetails);
-function isEven(num) {
-    return num % 2 === 0;
-}
-// console.log(isEven(11));
-const isDivisible = (num) => num % 4 === 0 && num % 8 === 0;
-// console.log(isDivisible(20));
 const bigNum1 = 9007199254740999n;
 const bigNum2 = 9007199254740991n;
 const difference = bigNum1 - bigNum2;
@@ -29,7 +23,14 @@ let vAny = 10; // We can assign anything to any
 let vUnknown = "10"; // We can assign anything to unknown just like any
 let s1 = vAny; // Any is assignable to anything
 // let s2: string = vUnknown; // it shows an error
-// optional parameter
+//! Function ..........................................................
+function isEven(num) {
+    return num % 2 === 0;
+}
+// console.log(isEven(11));
+const isDivisible = (num) => num % 4 === 0 && num % 8 === 0;
+// console.log(isDivisible(20));
+//* optional parameter
 function introduceMe(name, id) {
     return id
         ? `Hello, I am ${name} and my id is ${id}.`
@@ -37,11 +38,54 @@ function introduceMe(name, id) {
 }
 // console.log(introduceMe("Mobin"));
 // console.log(introduceMe("Mobin", 1130947042));
-// Array
+//! Array ..............................................................
 const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const arr2 = new Array(1, 3, 5, 7, 9);
 const arr3 = Array.of("S", "M", "T", "W", "T", "F", "S");
 const mapVal = arr1.map((el) => {
     return el % 2 === 0;
 });
-console.log(mapVal);
+// console.log(mapVal);
+//! Object .............................................................
+const person = {
+    name: "Mobin",
+    age: 33,
+    isDeveloper: true,
+    address: {
+        city: "Dhaka",
+        country: "Bangladesh",
+    },
+};
+const carYear = 2001;
+const carType = "Toyota";
+const carModel = "Corolla";
+const car = {
+    year: carYear,
+    type: carType,
+    model: carModel,
+};
+const person1 = {
+    name: "Mobin",
+    age: 33,
+    isDeveloper: true,
+    stack: "MERN",
+    address: {
+        city: "Dhaka",
+        country: "Bangladesh",
+    },
+};
+const person2 = {
+    name: "Abdullah",
+    age: 35,
+    isDeveloper: false,
+    address: {
+        city: "Dhaka",
+        country: "Bangladesh",
+    },
+};
+const student1 = {
+    name: "Arshia Tazri",
+    age: 8,
+    greet: (country) => `Hello, I am ${student1.name} from ${country}`,
+};
+console.log(student1.greet("Bangladesh"));
--- index.ts
+++ index.ts
@@ -17,13 +17,6 @@ const price: number = 2490;
 const productDetails: string = `The price of "${productCode}" is: ${price}`;
 // console.log(productDetails);
 
-function isEven(num: number): boolean {
-  return num % 2 === 0;
-}
-// console.log(isEven(11));
-const isDivisible = (num: number): boolean => num % 4 === 0 && num % 8 === 0;
-// console.log(isDivisible(20));
-
 const bigNum1: bigint = 9007199254740999n;
 const bigNum2: bigint = 9007199254740991n;
 const difference: bigint = bigNum1 - bigNum2;
@@ -35,7 +28,16 @@ let vUnknown: unknown = "10"; // We can assign anything to unknown just like any
 let s1: string = vAny; // Any is assignable to anything
 // let s2: string = vUnknown; // it shows an error
 
-// optional parameter
+//! Function ..........................................................
+function isEven(num: number): boolean {
+  return num % 2 === 0;
+}
+// console.log(isEven(11));
+const isDivisible: (num: number) => boolean = (num: number): boolean =>
+  num % 4 === 0 && num % 8 === 0;
+// console.log(isDivisible(20));
+
+//* optional parameter
 function introduceMe(name: string, id?: number): string {
   return id
     ? `Hello, I am ${name} and my id is ${id}.`
@@ -45,7 +47,7 @@ function introduceMe(name: string, id?: number): string {
 // console.log(introduceMe("Mobin"));
 // console.log(introduceMe("Mobin", 1130947042));
 
-// Array
+//! Array ..............................................................
 const arr1: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const arr2: number[] = new Array(1, 3, 5, 7, 9);
 const arr3: string[] = Array.of("S", "M", "T", "W", "T", "F", "S");
@@ -54,3 +56,83 @@ const mapVal: boolean[] = arr1.map((el: number) => {
   return el % 2 === 0;
 });
 // console.log(mapVal);
+
+//! Object .............................................................
+const person: {
+  name: string;
+  age: number;
+  isDeveloper: boolean;
+  address: { city: string; country: string };
+} = {
+  name: "Mobin",
+  age: 33,
+  isDeveloper: true,
+  address: {
+    city: "Dhaka",
+    country: "Bangladesh",
+  },
+};
+
+//? type alias .................................................
+//Type Aliases allow defining types with a custom name
+type CarYear = number;
+type CarType = string;
+type CarModel = string;
+
+type Car = {
+  year: CarYear;
+  type: CarType;
+  model: CarModel;
+};
+
+const carYear: CarYear = 2001;
+const carType: CarType = "Toyota";
+const carModel: CarModel = "Corolla";
+
+const car: Car = {
+  year: carYear,
+  type: carType,
+  model: carModel,
+};
+
+type Person = {
+  name: string;
+  age: number;
+  isDeveloper: boolean;
+  stack?: string; //*optional property
+  address: { city: string; country: string };
+};
+const person1: Person = {
+  name: "Mobin",
+  age: 33,
+  isDeveloper: true,
+  stack: "MERN",
+  address: {
+    city: "Dhaka",
+    country: "Bangladesh",
+  },
+};
+const person2: Person = {
+  name: "Abdullah",
+  age: 35,
+  isDeveloper: false,
+  address: {
+    city: "Dhaka",
+    country: "Bangladesh",
+  },
+};
+
+//? call signatures ........................................
+// call signature is simply define type of a function in an object type notation
+type Student = {
+  name: string;
+  age: number;
+  greet: (country: string) => string; // method call signature
+};
+
+const student1: Student = {
+  name: "Arshia Tazri",
+  age: 8,
+  greet: (country): string => `Hello, I am ${student1.name} from ${country}`,
+};
+console.log(student1.greet("Bangladesh"));
